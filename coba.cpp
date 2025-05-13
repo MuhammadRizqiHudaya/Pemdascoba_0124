92567ddfe343198a7266cc066481f2adccdddbe7
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-05-13 18:23:32 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi class BangunDatar
+class BangunDatar;
+
+class PersegiPanjang {
+public:
+    void inputData(BangunDatar &bd);
+    void outputData(BangunDatar &bd);
+};
+
+class BangunDatar {
+private:
+    float panjang;
+    float lebar;
+
+    float hitungLuas() {
+        return panjang * lebar;
+    }
+
+    float hitungKeliling() {
+        return 2 * (panjang + lebar);
+    }
+
+public:
+    // Untuk mengakses panjang dan lebar
+    friend void PersegiPanjang::inputData(BangunDatar &bd);
+    // Untuk mengakses fungsi
+    friend void PersegiPanjang::outputData(BangunDatar &bd);
+};
+
+// Definisi fungsi inputData dari class PersegiPanjang
+void PersegiPanjang::inputData(BangunDatar &bd) {
+    cout << "Masukkan Panjang : ";
+    cin >> bd.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> bd.lebar;
+}
+
+// Definisi fungsi outputData dari class PersegiPanjang
+void PersegiPanjang::outputData(BangunDatar &bd) {
+    cout << "Luas     : " << bd.hitungLuas() << endl;
+    cout << "Keliling : " << bd.hitungKeliling() << endl;
+}
+
+int main() {
+    PersegiPanjang pP;
+    BangunDatar bD;
+
+    pP.inputData(bD);
+    pP.outputData(bD);
+
+    return 0;
+}
